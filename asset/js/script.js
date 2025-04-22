288b598a52b120763ff291bc72ae4af99ef803bb
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2025-04-22 22:53:19 +0600

    hero section is done

--- asset/js/script.js
+++ asset/js/script.js
@@ -15,4 +15,35 @@ document.addEventListener('DOMContentLoaded', function () {
         effect: "cube"
     });
 
+    // text-typed-auto 
+    var typed = new Typed('.typed', {
+        // Waits 1000ms after typing "First"
+        strings: ['front-End developer.', 'full stack web developer.', 'front-End developer.',],
+        typeSpeed: 50,
+        loop: true,
+        backDelay: 900,
+        backSpeed: 30,
+    });
+
+    // scroll-top
+
+    const home = document.querySelector("#home");
+
+    const footerElem = document.querySelector(".footer-section");
+
+    const scrollElement = document.createElement('div');
+
+    scrollElement.classList.add("scrollTop-style");
+
+    scrollElement.innerHTML = `<span class="scroll-top"><i class="fa-solid fa-arrow-up"></i></span>`;
+
+    footerElem.after(scrollElement);
+
+    const scrollTop = () =>{
+    home.scrollIntoView({behavior: "smooth"});
+    };
+
+    scrollElement.addEventListener('click',scrollTop);
+
+
 });
